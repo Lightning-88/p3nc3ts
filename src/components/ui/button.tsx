91014ed2e856b2c014103f0ec0c0b5f412e7f853
lightning-88 <erlanggasatria6677@gmail.com>
2026-01-13 21:02:28 +0700

    fixes button and input group components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,16 +1,23 @@
 "use client";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary";
+}
 
 export function Button({
   children,
   className,
   disabled,
+  variant = "primary",
   ...props
 }: ButtonProps) {
   return (
     <button
-      className={`bg-secondary py-2 px-4 text-primary rounded-md text-sm hover:bg-hover disabled:bg-disabled transition-colors duration-75 ${
+      className={`${
+        variant === "primary"
+          ? "bg-secondary text-primary hover:bg-hover disabled:bg-disabled"
+          : "bg-primary text-secondary hover:bg-zinc-300 disabled:bg-zinc-400"
+      } py-2 px-4 rounded-md text-sm transition-colors duration-75 ${
         className ?? ""
       }`}
       disabled={disabled}
--- src/components/ui/input-group.tsx
+++ src/components/ui/input-group.tsx
@@ -1,5 +1,3 @@
 export function InputGroup({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="grid w-full max-w-sm items-center gap-2">{children}</div>
-  );
+  return <div className="grid w-full items-center gap-2">{children}</div>;
 }
