cdc40bec648dab413eebbbd16502c1b3b5c1f50e
lightning-88 <erlanggasatria6677@gmail.com>
2026-01-14 18:32:43 +0700

    add middleware for verification

---
+++ src/proxy.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { decrypt } from "./lib/auth/jwt";
+
+export async function proxy(request: NextRequest) {
+  const session = request.cookies.get("s")?.value;
+
+  const result = await decrypt(session);
+
+  if (!result?.userId)
+    return NextResponse.redirect(
+      new URL("/login?err=login first", request.nextUrl)
+    );
+
+  const next = NextResponse.next();
+  next.headers.set("x-user-id", String(result.userId));
+
+  return next;
+}
+
+export const config = {
+  matcher: ["/profile/:path*", "/notifications/:path*"],
+};
