a802704bd26bcd4db56e426e511390d84b3f6604
lightning-88 <erlanggasatria6677@gmail.com>
2026-01-14 18:49:54 +0700

    create utility function for get user

---
+++ src/lib/db/users.ts
@@ -0,0 +1,33 @@
+import { prismaClient } from "@/lib/db/prisma";
+import { cookies } from "next/headers";
+import { decrypt } from "../auth/jwt";
+
+export async function getUser() {
+  const session = (await cookies()).get("s")?.value;
+  const result = await decrypt(session);
+  if (!result?.userId) return null;
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: String(result.userId),
+    },
+  });
+
+  return user;
+}
+
+export async function getUserById(id: string) {
+  return await prismaClient.user.findUnique({
+    where: {
+      id,
+    },
+  });
+}
+
+export async function getUserId() {
+  const session = (await cookies()).get("s")?.value;
+  const result = await decrypt(session);
+  if (!result?.userId) return null;
+
+  return String(result.userId);
+}
