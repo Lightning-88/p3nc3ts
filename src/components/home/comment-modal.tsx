704066f41001415a2576ed59833c0e775167d3a6
lightning-88 <erlanggasatria6677@gmail.com>
2026-01-16 19:17:09 +0700

    create comments modal

---
+++ src/components/home/comment-modal.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { X } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect } from "react";
+
+type CommentData = {
+  id: string;
+  createdAt: Date;
+  authorId: string;
+  content: string;
+  postId: string;
+  author: {
+    id: string;
+    username: string;
+    email: string;
+    name: string;
+    password: string;
+    bio: string | null;
+    location: string | null;
+    photo: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+};
+
+export function CommentModal({
+  comments,
+  onClose,
+}: {
+  comments: CommentData[];
+  onClose: () => void;
+}) {
+  useEffect(() => {
+    if (comments) {
+      document.body.style.overflow = "hidden";
+    }
+
+    return () => {
+      document.body.style = "";
+    };
+  }, [comments]);
+
+  return (
+    <div className="fixed flex justify-center items-center h-dvh top-0 right-0 bottom-0 left-0 bg-[rgba(0,0,0,0.5)] z-10">
+      <div className="bg-primary w-full max-w-md rounded-md space-y-4 mx-4 min-h-[300px] max-h-[80vh] overflow-y-auto">
+        <div className="flex justify-between border-b border-border-primary p-4 mb-4">
+          <h2 className="text-lg font-bold">Comments</h2>
+          <button onClick={onClose}>
+            <X />
+          </button>
+        </div>
+
+        {comments.length === 0 ? (
+          <div className="flex items-center justify-center min-h-[200px]">
+            No comments yet.
+          </div>
+        ) : (
+          <div className="space-y-4 pt-0 p-4">
+            {comments.map((comment) => (
+              <div
+                key={comment.id}
+                className="inset-shadow-xs shadow-xs p-4 space-y-2 rounded-md"
+              >
+                <div className="flex items-center">
+                  <Link
+                    href={`/profile/${comment.author.username}`}
+                    className="inline-block mr-4"
+                  >
+                    <Image
+                      src={`https://ui-avatars.com/api/?name=${encodeURIComponent(
+                        comment.author.photo
+                          ? comment.author.photo
+                          : comment.author.username
+                      )}`}
+                      alt="profile"
+                      className="rounded-full"
+                      width={44}
+                      height={44}
+                      unoptimized
+                    />
+                  </Link>
+
+                  <div className="flex flex-col justify-center">
+                    <div>
+                      <Link
+                        href={`/profile/${comment.author.username}`}
+                        className="inline-block font-bold text-sm"
+                      >
+                        {comment.author.name}
+                      </Link>
+                    </div>
+                    <div className="text-disabled text-xs">
+                      {comment.createdAt.toLocaleString("sv-SE")}
+                    </div>
+                  </div>
+                </div>
+                <p className="text-sm">{comment.content}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- src/components/home/post-card.tsx
+++ src/components/home/post-card.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import { ExpandableText } from "../ui/expandeble-text";
 import Link from "next/link";
+import { Heart, LucideShare2, MessageCircle } from "lucide-react";
+import { useState } from "react";
+import { CommentModal } from "./comment-modal";
 
 type PostData = {
   id: string;
@@ -22,58 +27,103 @@ type PostData = {
     createdAt: Date;
     updatedAt: Date;
   };
+  comments: {
+    id: string;
+    createdAt: Date;
+    authorId: string;
+    content: string;
+    postId: string;
+    author: {
+      id: string;
+      username: string;
+      email: string;
+      name: string;
+      password: string;
+      bio: string | null;
+      location: string | null;
+      photo: string | null;
+      createdAt: Date;
+      updatedAt: Date;
+    };
+  }[];
 };
 
-export default function PostCard({ post }: { post: PostData }) {
+export function PostCard({ post }: { post: PostData }) {
+  const [activeModal, setActiveModal] = useState<string | null>(null);
+
   return (
-    <div className="inset-shadow-xs rounded-md space-y-4 border border-border-primary p-4">
-      <div className="flex items-center justify-between">
-        <div className="flex">
-          <Link
-            href={`/profile/${post.author.username}`}
-            className="inline-block mr-4"
-          >
-            <Image
-              src={`https://ui-avatars.com/api/?name=${encodeURIComponent(
-                post?.author.photo ? post.author.photo : post.author.username
-              )}`}
-              alt="profile"
-              className="rounded-full"
-              width={48}
-              height={48}
-              unoptimized
-            />
-          </Link>
-          <div className="flex flex-col justify-center">
-            <div>
-              <Link
-                href={`/profile/${post.author.username}`}
-                className="inline-block font-bold"
-              >
-                {post.author.name}
-              </Link>
-            </div>
-            <div className="text-disabled text-xs">
-              {post.createdAt.toLocaleString("sv-SE")}
+    <>
+      <div className="inset-shadow-xs rounded-md space-y-4 border border-border-primary p-4">
+        <div className="flex items-center justify-between">
+          <div className="flex">
+            <Link
+              href={`/profile/${post.author.username}`}
+              className="inline-block mr-4"
+            >
+              <Image
+                src={`https://ui-avatars.com/api/?name=${encodeURIComponent(
+                  post?.author.photo ? post.author.photo : post.author.username
+                )}`}
+                alt="profile"
+                className="rounded-full"
+                width={48}
+                height={48}
+                unoptimized
+              />
+            </Link>
+            <div className="flex flex-col justify-center">
+              <div>
+                <Link
+                  href={`/profile/${post.author.username}`}
+                  className="inline-block font-bold"
+                >
+                  {post.author.name}
+                </Link>
+              </div>
+              <div className="text-disabled text-xs">
+                {post.createdAt.toLocaleString("sv-SE")}
+              </div>
             </div>
           </div>
         </div>
+
+        <ExpandableText text={post.content} />
+
+        {post.photo && (
+          <div className="relative w-full overflow-hidden rounded-xl border border-border-primary aspect-square">
+            <Image
+              alt={post.id}
+              src={post.photo}
+              fill
+              priority
+              unoptimized
+              className="object-cover transition-transform duration-300 hover:scale-105"
+            />
+          </div>
+        )}
+
+        <div className="flex gap-2">
+          <button className="flex gap-1">
+            <Heart /> {0}
+          </button>
+          <button
+            className="flex gap-1"
+            onClick={() => setActiveModal("comment")}
+          >
+            <MessageCircle size={22} /> {post.comments.length}
+          </button>
+          <button className="flex gap-1">
+            <LucideShare2 size={22} />
+          </button>
+        </div>
       </div>
 
-      <ExpandableText text={post.content} />
-
-      {post.photo && (
-        <div className="relative w-full overflow-hidden rounded-xl border border-border-primary aspect-square">
-          <Image
-            alt="image post media"
-            src={post.photo}
-            fill
-            priority
-            unoptimized
-            className="object-cover transition-transform duration-300 hover:scale-105"
-          />
-        </div>
+      {activeModal === "comment" && (
+        <CommentModal
+          comments={post.comments}
+          onClose={() => setActiveModal(null)}
+        />
       )}
-    </div>
+    </>
   );
 }
