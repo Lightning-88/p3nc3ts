4e163fad25a177c53dc43345580ff8a58b7e3c57
lightning-88 <erlanggasatria6677@gmail.com>
2026-01-16 20:01:26 +0700

    make profile page little bit better

--- src/app/(main)/profile/[profileUsername]/page.tsx
+++ src/app/(main)/profile/[profileUsername]/page.tsx
@@ -1,13 +1,55 @@
+import { getUserByUsername } from "@/lib/db/users";
+import { MapPin } from "lucide-react";
+import Image from "next/image";
+
 export default async function OtherProfilePage({
   params,
 }: {
   params: Promise<{ profileUsername: string }>;
 }) {
   const { profileUsername } = await params;
+  const user = await getUserByUsername(profileUsername);
+
+  if (!user) {
+    return <div>User not found</div>;
+  }
 
   return (
     <div>
-      <h1>{profileUsername}</h1>
+      <div className="p-4 space-y-2">
+        <div className="h-24 w-24">
+          <Image
+            src={`https://ui-avatars.com/api/?name=${encodeURIComponent(
+              user.photo ? user.photo : user.username
+            )}`}
+            alt="profile"
+            className="rounded-full"
+            width={96}
+            height={96}
+            unoptimized
+          />
+        </div>
+
+        <div>
+          <h1 className="text-xl font-bold">{user.name || user.username}</h1>
+          <p className="text-disabled">@{user.username}</p>
+          <p className="leading-normal">{user.bio ?? "No bio available"}</p>
+        </div>
+
+        <div className="flex justify-between items-center text-sm text-disabled">
+          <div className="flex gap-1">
+            <MapPin size={20} /> {user.location ?? "Unknown"}
+          </div>
+          <div className="flex gap-4 text-sm">
+            <div>
+              <span className="font-bold">420</span> <span>Following</span>
+            </div>
+            <div>
+              <span className="font-bold">1.2K</span> <span>Followers</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
